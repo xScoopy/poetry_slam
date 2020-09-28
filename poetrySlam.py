e9ef7a8dba0ec0c3386522f7e3b40515ff71c32e
xScoopy <JLeary070@gmail.com>
2020-09-28 16:09:52 -0700

    added a function to add the original line number to the poem lines

--- poetrySlam.py
+++ poetrySlam.py
@@ -11,12 +11,14 @@ def lines_printed_backwards(lines_list):
 #Creation of lines_printed_random() function
 def lines_printed_random(lines_list):
     while len(lines_list) > 0: 
-        
-        #for i in range(len(lines_list)):
         print(f'{lines_list.pop(random.randint(0, len(lines_list)-1))}')
-        
+
 #Creation of add line number to line function
+def add_line_number(lines_list):
+    for i in range(len(lines_list)):
+        lines_list[i] = f'{i} {lines_list[i]}'
+    return lines_list
+        
 
 
-
-lines_printed_random(get_file_lines('poem.txt'))
+print(add_line_number(get_file_lines('poem.txt')))
